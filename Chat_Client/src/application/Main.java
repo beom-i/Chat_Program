2c1c1cc6a4b3253149b0eda8812080dea7104051
beom-i <lybgreen77@naver.com>
2019-12-18 01:12:41 +0900

    Code GUI of Main code of Chat Client

--- Chat_Client/src/application/Main.java
+++ Chat_Client/src/application/Main.java
@@ -87,6 +87,91 @@ public class Main extends Application {
 		};
 		thread.start();
 	}
+	@Override
+	public void start(Stage primaryStage) {
+		BorderPane root = new BorderPane();
+		root.setPadding(new Insets(5));
+		
+		HBox hbox = new HBox();
+		hbox.setSpacing(5);
+		
+		TextField userName = new TextField();
+		userName.setPrefWidth(150);
+		userName.setPromptText("write name");
+		HBox.setHgrow(userName,Priority.ALWAYS);
+		
+		TextField IPText = new TextField("127.0.0.1");
+		TextField portText = new TextField("9876");
+		portText.setPrefWidth(80);
+		
+		hbox.getChildren().addAll(userName,IPText,portText);
+		root.setTop(hbox);
+		
+		textArea = new TextArea();
+		textArea.setEditable(false);
+		root.setCenter(textArea);
+		
+		TextField input = new TextField();
+		input.setPrefWidth(Double.MAX_VALUE);
+		input.setDisable(true);
+		
+		input.setOnAction(event -> {
+			send(userName.getText() + ": " + input.getText() + "\n");
+			input.setText("");
+			input.requestFocus();
+		});
+		
+		Button sendButton = new Button("send");
+		sendButton.setDisable(true);
+		sendButton.setOnAction(event -> {
+			send(userName.getText() + ": " + input.getText() + "\n");
+			input.setText("");
+			input.requestFocus();
+		});	
+		
+		Button connectionButton = new Button("connect");
+		connectionButton.setOnAction(event -> {
+			if(connectionButton.getText().equals("connect")) {
+				int port = 9876;
+				try {
+					port = Integer.parseInt(portText.getText());
+				} catch(Exception e) {
+					e.printStackTrace();
+				}
+				startClient(IPText.getText(),port);
+				Platform.runLater(() -> {
+					textArea.appendText("[ chat room connect ]\n");
+				});
+				connectionButton.setText("bye");
+				input.setDisable(false);
+				sendButton.setDisable(false);
+				input.requestFocus();
+			} else {
+				stopClient();
+				Platform.runLater(() -> {
+					textArea.appendText("[chat room bye]\n");
+				});
+				connectionButton.setText("connect");
+				input.setDisable(true);
+				sendButton.setDisable(true);
+			}
+		});
+		
+		BorderPane pane = new BorderPane();
+		pane.setLeft(connectionButton);
+		pane.setCenter(input);
+		pane.setRight(sendButton);
+		
+		root.setBottom(pane);
+		
+		Scene scene = new Scene(root,400,400);
+		primaryStage.setTitle("[chat client]");
+		primaryStage.setScene(scene);
+		primaryStage.setOnCloseRequest(event -> stopClient());
+		primaryStage.show();
+		
+		connectionButton.requestFocus();
+	}
 	public static void main(String[] args) {
 		launch(args);
 	}
